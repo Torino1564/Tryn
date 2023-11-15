484e6911c264c5cf2bd15052fbc412e2431eedd3
Torino1564 <jtorinor6@gmail.com>
2023-11-15 00:30:56 -0300

    Sync

--- WindowSandbox/TestApp.cpp
+++ WindowSandbox/TestApp.cpp
@@ -33,12 +33,13 @@ public:
 			for (auto pointLight : pPointLights)
 			{
 				pointLight->Bind();
-				GetRenderQueueByID("Lambertian").RunJobsAsync(gfx,master,workerPtrs);
+				GetRenderQueueByID("Lambertian").RunJobsAsync(gfx, master, workerPtrs);
+				//GetRenderQueueByID("Lambertian").RunJobs(gfx);
 			}
 		}
 	}
 private:
-	static constexpr int workerNumber = 1;
+	static constexpr int workerNumber = 6;
 	ccr::Master master;
 	std::vector<std::unique_ptr<gfx::RenderWorker>> workerPtrs;
 };
@@ -95,7 +96,7 @@ TestApp::TestApp(std::shared_ptr<win::IWindow> wnd, std::shared_ptr<gfx::IGraphi
 			}
 		}
 	}
-
+	entities.emplace_back(std::make_unique<ent::BasicEntity>(Gfx(), "sponza", "resources/models/Sponza/sponza.obj", glm::vec3{ 0.01f,0.01f,0.01f }));
 	pPointLight = std::make_unique<gfx::PointLight>(Gfx(), 0.01f);
 
 	this->wnd->keyboard.DisableAutoRepeat();
